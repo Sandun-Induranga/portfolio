53f42d3c39b8878e96746e66f3d8e4559e6aa6d6
Sandun Induranga <sandutharu40@gmail.com>
2024-12-29 22:59:41 +0530

    Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+
+const Navbar = () => {
+  return (
+    <AppBar position="static" style={{ backgroundColor: '#121212' }}>
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          Sandu
+        </Typography>
+        <Box display="flex" gap="1rem">
+          {['Home', 'About', 'Portfolio', 'Blog', 'Contact'].map((item) => (
+            <Button key={item} color="inherit">
+              {item}
+            </Button>
+          ))}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
